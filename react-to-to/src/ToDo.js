b0c91bcc5c3eedc5f6f44e9e795b513425d27079
Highspeed1k <spencerpaulbrown@gmail.com>
2018-08-13 16:46:41 -0500

    to do assignment done

--- react-to-to/src/ToDo.js
+++ react-to-to/src/ToDo.js
@@ -5,12 +5,10 @@ import ToDo from './components/ToDo.js';
 class ToDo extends Component {
   render() {
   return (
-    <div className="App">
-      <ul>
-        <ToDo />
-        <ToDo />
-      </ul>
-     </div>
+    <li>
+         <input type="checkbox" checked={ this.props.isCompleted } />
+         <span>{ this.props.description }</span>
+       </li>
 
   );
 }
