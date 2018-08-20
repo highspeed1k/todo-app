444487e7e93705e3d0ccaf9a3341e79ca04dd019
Highspeed1k <spencerpaulbrown@gmail.com>
2018-08-20 16:25:35 -0500

    commit

--- react-to-to/src/ToDo.js
+++ react-to-to/src/ToDo.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
-import ToDo from './components/ToDo.js';
+
 
 class ToDo extends Component {
   render() {
-  return (
-    <li>
-         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-         <span>{ this.props.description }</span>
-       </li>
-
-  );
+    return (
+        <li>
+           <input type="checkbox" checked={ this.props.isCompleted } />
+           <span>{ this.props.description }</span>
+        </li>
+    );
+  }
 }
-}
 
- export default ToDo;
+export default ToDo;
